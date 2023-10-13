f9a5678b6c5a7e64cc1443322e6d58603823f9b6
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-10-14 00:06:45 +0300

    adaugarea functionalitate de login si inregistrare

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,94 @@
+const { prisma } = require("../prisma/prisma-client");
+const brypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+/**
+ * @route  POST /api/user/Login
+ * @desc Login
+ * @access Public
+ */
+
 const login = async (req, res) => {
-    res.send("login");
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res
+            .status(400)
+            .json({ message: "Te rog, umpleti campurile obligatorii" });
+    }
+
+    const user = await prisma.user.findFirst({
+        where: {
+            email,
+        },
+    });
+
+    const isPasswordCorrect =
+        user && (await brypt.compare(password, user.password));
+    const secret = process.env.JWT_SECRET;
+
+    if (user && isPasswordCorrect && secret) {
+        res.status(200).json({
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            token: jwt.sign({ id: user.id }, secret, { expiresIn: "30d" }),
+        });
+    } else {
+        return res
+            .status(400)
+            .json({ message: "Nu este corect loghin sau parola" });
+    }
 };
 
+/**
+ * @route POST/api/user/register
+ * @desc inregistrare
+ * @access Public
+ */
 const register = async (req, res) => {
-    res.send("register");
+    const { email, password, name } = req.body;
+
+    if (!email || !password || !name) {
+        return res
+            .status(400)
+            .json({ message: "Te rog, umpleti campurile obligatorii" });
+    }
+
+    const registeredUser = await prisma.user.findFirst({
+        where: {
+            email,
+        },
+    });
+
+    if (registeredUser) {
+        return res
+            .status(400)
+            .json({ message: "Utilizator, cu asa email deja este" });
+    }
+
+    const salt = await brypt.genSalt(10);
+    const hashedPassord = await brypt.hash(password, salt);
+
+    const user = await prisma.user.create({
+        data: {
+            email,
+            name,
+            password: hashedPassord,
+        },
+    });
+
+    const secret = process.env.JWT_SECRET;
+
+    if (user && secret) {
+        res.status(201).json({
+            is: user.id,
+            email: user.email,
+            name,
+            token: jwt.sign({ id: user.id }, secret, { expiresIn: "30d" }),
+        });
+    } else {
+        return res.status(400).json({ message: "Nu sa putut  crea utilizator" });
+    }
 };
 
 const current = async (req, res) => {
@@ -13,5 +98,5 @@ const current = async (req, res) => {
 module.exports = {
     login,
     register,
-    current
-}
+    current,
+};
