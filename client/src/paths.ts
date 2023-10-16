b21aac259c9093dcbbb0adad65ae35c44d8aa441
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-10-16 18:15:46 +0300

    add maps paths and file paths.ts with route

---
+++ client/src/paths.ts
@@ -0,0 +1,9 @@
+export const Paths = {
+    home: '/',
+    employeeAdd: '/employee/add',
+    employeeEdit: '/employee/edit',
+    employee: '/employee',
+    status: '/staus',
+    login: '/login',
+    register: '/register',
+} as const;
