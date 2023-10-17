54ed0ad907ae4e7b57e0aa82fbe41b00c0492dbd
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-10-17 17:53:00 +0300

    add link to route home | register | login

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,16 +1,28 @@
-import { Layout, Space, Typography, Button } from "antd";
+import { Layout, Space, Typography } from "antd";
 import { TeamOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 import { CustomButton } from "../custom-button";
 import styles from "./index.module.css";
+import { Paths } from "../../paths";
 
 export const Header = () => {
   return (
     <Layout.Header className={styles.header}>
       <Space>
         <TeamOutlined className={styles.teamIcon} />
-        <CustomButton type='ghost'>
-          <Typography.Title level={1}> Utilizatori</Typography.Title>
-        </CustomButton>
+        <Link to={Paths.home}>
+          <CustomButton type="ghost">
+            <Typography.Title level={1}> Utilizatori</Typography.Title>
+          </CustomButton>
+        </Link>
+      </Space>
+      <Space>
+        <Link to={Paths.register}>
+          <CustomButton type="ghost">Inregistrare</CustomButton>
+        </Link>
+        <Link to={Paths.login}>
+          <CustomButton type="ghost">Logare</CustomButton>
+        </Link>
       </Space>
     </Layout.Header>
   );
