3464063e5134e6f1d90e0b0649ed9f034fcc845e
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-10-17 19:34:24 +0300

    add input component for the login page

---
+++ client/src/components/costum-input/index.tsx
@@ -0,0 +1,15 @@
+import { Form, Input } from "antd";
+
+type Props = {
+  name: string;
+  placeholder: string;
+  type?: string;
+};
+
+export const CustomInput = ({name, placeholder, type = "text"}: Props) => {
+  return (
+    <Form.Item name={name} rules={[{required: true, message: 'Camp obligatoriu'}]} shouldUpdate={true}>
+      <Input placeholder={placeholder} type={type} size="large" />
+    </Form.Item>
+  );
+};
