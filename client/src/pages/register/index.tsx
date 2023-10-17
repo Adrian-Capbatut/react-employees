426964b7b539634b3e61bc155a3e863c2233d9de
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-10-18 01:26:24 +0300

    finished page register

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Layout } from "../../components/layout";
+import { Row, Card, Form, Space, Typography } from "antd";
+import { CustomInput } from "../../components/costum-input";
+import { PasswordInput } from "../../components/password-input";
+import { CustomButton } from "../../components/custom-button";
+import { Link } from "react-router-dom";
+import { Paths } from "../../paths";
 
 export const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <Layout>
+      <Row align="middle" justify="center">
+        <Card title="Inregistrativa" style={{ width: "30rem" }}>
+          <Form onFinish={() => null}>
+            <CustomInput type="name" placeholder="Nume" />
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Parola" />
+            <PasswordInput
+              name="confirmPassword"
+              placeholder="Repetati parola"
+            />
+            <CustomButton type="primary" htmlType="submit">
+              Inregistrativa
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Deja sunteti inregistrat ? <Link to={Paths.login}>Intrati</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
+    </Layout>
+  );
+};
