4d59a613e833547c6399406a3c7d3ce1926e5b37
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-10-18 01:12:27 +0300

    finis page login

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Layout } from "../../components/layout";
-import { Row, Card, Form } from "antd";
+import { Row, Card, Form, Space, Typography } from "antd";
+import { CustomInput } from "../../components/costum-input";
+import { PasswordInput } from "../../components/password-input";
+import { CustomButton } from "../../components/custom-button";
+import { Link } from "react-router-dom"
+import { Paths } from "../../paths";
 
 export const Login = () => {
   return (
@@ -8,8 +13,17 @@ export const Login = () => {
       <Row align="middle" justify="center">
         <Card title="Intrati" style={{ width: "30rem" }}>
           <Form onFinish={() => null}>
-            
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Parola" />
+            <CustomButton type="primary" htmlType="submit">
+              Intrati
+            </CustomButton>
           </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Nu aveti profil? <Link to={Paths.register}>Inregistrativa</Link>
+            </Typography.Text>
+          </Space>
         </Card>
       </Row>
     </Layout>
