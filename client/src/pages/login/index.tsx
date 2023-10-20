5fec3a457b0e6ddd0bc0621b2213835048e46180
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-10-20 14:11:16 +0300

    redirection to the employees page

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -4,21 +4,25 @@ import { Row, Card, Form, Space, Typography } from "antd";
 import { CustomInput } from "../../components/costum-input";
 import { PasswordInput } from "../../components/password-input";
 import { CustomButton } from "../../components/custom-button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Paths } from "../../paths";
 import { UserData, useLoginMutation } from "../../app/services/auth";
 import { isErrorWithMessage } from "../../utils/is-error-with-message";
 import { ErrorMessage } from "../../components/error-message";
 
 export const Login = () => {
+  const navigate = useNavigate();
   const [loginUser, loginUserResult] = useLoginMutation();
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const login = async (data: UserData) => {
     try {
       await loginUser(data).unwrap();
+      navigate("/")
+      
     } catch (err) {
       const maybeError = isErrorWithMessage(err);
+
 
       if (maybeError) {
         setError(err.data.message);
