0bd61cc5a0f32aca0f3031993140eee04af1383a
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-10-20 14:03:43 +0300

    Implemented User Login Functionality with Error Handling

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import { useState } from "react";
 import { Layout } from "../../components/layout";
 import { Row, Card, Form, Space, Typography } from "antd";
 import { CustomInput } from "../../components/costum-input";
 import { PasswordInput } from "../../components/password-input";
 import { CustomButton } from "../../components/custom-button";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { Paths } from "../../paths";
+import { UserData, useLoginMutation } from "../../app/services/auth";
+import { isErrorWithMessage } from "../../utils/is-error-with-message";
+import { ErrorMessage } from "../../components/error-message";
 
 export const Login = () => {
+  const [loginUser, loginUserResult] = useLoginMutation();
+  const [error, setError] = useState('');
+
+  const login = async (data: UserData) => {
+    try {
+      await loginUser(data).unwrap();
+    } catch (err) {
+      const maybeError = isErrorWithMessage(err);
+
+      if (maybeError) {
+        setError(err.data.message);
+      } else {
+        setError("Eroare neidentificata");
+      }
+    }
+  };
+
   return (
     <Layout>
       <Row align="middle" justify="center">
         <Card title="Intrati" style={{ width: "30rem" }}>
-          <Form onFinish={() => null}>
+          <Form onFinish={login}>
             <CustomInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Parola" />
             <CustomButton type="primary" htmlType="submit">
@@ -23,6 +43,7 @@ export const Login = () => {
             <Typography.Text>
               Nu aveti profil? <Link to={Paths.register}>Inregistrativa</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </Card>
       </Row>
