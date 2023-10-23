fd4f624a41a54cbfaf854917f9ba0145b23f9300
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-10-23 18:13:02 +0300

    add the function that adds users

--- client/src/pages/employees/index.tsx
+++ client/src/pages/employees/index.tsx
@@ -40,11 +40,13 @@ export const Employees = () => {
     }
   }, [navigate, user]);
 
+  const goToAddUser = () => navigate(Paths.employeeAdd)
+
   return (
     <Layout>
       <CustomButton
         type="primary"
-        onClick={() => null}
+        onClick={ goToAddUser }
         icon={<PlusCircleOutlined />}>
         Adaugati
       </CustomButton>
