6a4a416ad968382bcde82f038bc62c647896191b
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-10-23 16:51:10 +0300

    Importing the employees pgae

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -10,11 +10,12 @@ import { Login } from "./pages/login";
 import { Register } from "./pages/register";
 import "./index.css";
 import { Auth } from "./features/auth/auth";
+import { Employees } from "./pages/employees";
 
 const router = createBrowserRouter([
   {
     path: Paths.home,
-    element: <h1>Employees</h1>,
+    element: <Employees/>,
   },
   {
     path: Paths.login,
