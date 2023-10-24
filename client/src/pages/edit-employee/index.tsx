35fa9454480c9c9690493e96d4834dca4bbe0756
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-10-25 01:24:09 +0300

    Added employee edit page

---
+++ client/src/pages/edit-employee/index.tsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useEditEmployeesMutation, useGetEmployeesQuery } from '../../app/services/employees';
+import { Layout } from '../../components/layout';
+import { Row } from 'antd';
+import { EmployeeForm } from '../../components/employee-form';
+import { Employee } from '@prisma/client';
+import { Paths } from '../../paths';
+import { isErrorWithMessage } from '../../utils/is-error-with-message';
+
+export const EditEmployee = () => {
+
+    const navigate = useNavigate();
+    const params = useParams<{id: string}>();
+    const [error, setError] = useState('');
+    const {data, isLoading} = useGetEmployeesQuery(params.id || "");
+    const [editEmployee] = useEditEmployeesMutation();
+
+    if (isLoading) {
+        return <span>Se incarca</span>
+    }
+
+
+    return(
+<Layout>
+    <Row align="middle" justify="center">
+        <EmployeeForm 
+        title='Redactati angajatul'
+        btnText='Redactati'
+        error={error}
+        employee={data}
+        onFinish={handleEditUser}
+        />
+
+    </Row>
+</Layout>
+    )
+
+}
