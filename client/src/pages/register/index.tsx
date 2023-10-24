5b5af88dbab7bd0257daac4cf8a112f3c0ffae6f
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-10-25 02:46:33 +0300

    add functionality to registration

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "../../components/layout";
 import { Row, Card, Form, Space, Typography } from "antd";
 import { CustomInput } from "../../components/costum-input";
 import { PasswordInput } from "../../components/password-input";
 import { CustomButton } from "../../components/custom-button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Paths } from "../../paths";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/auth/authSlice";
+import { useRegisterMutation } from "../../app/services/auth";
+import { User } from "@prisma/client";
+import { isErrorWithMessage } from "../../utils/is-error-with-message";
+import { ErrorMessage } from "../../components/error-message";
+
+type RegisterData = Omit<User, "id"> & { confirmPassword: string };
 
 export const Register = () => {
+  const navigate = useNavigate();
+  const user = useSelector(selectUser);
+  const [error, setError] = useState("");
+  const [registerUser] = useRegisterMutation();
+
+  const register = async (data: RegisterData) => {
+    try {
+      await registerUser(data).unwrap();
+
+      navigate("/");
+    } catch (error) {
+      const maybeError = isErrorWithMessage(error);
+
+      if (maybeError) {
+        setError(error.data.message);
+      } else {
+        setError("Eroare neidentificata");
+      }
+    }
+  };
+
   return (
     <Layout>
       <Row align="middle" justify="center">
         <Card title="Inregistrativa" style={{ width: "30rem" }}>
-          <Form onFinish={() => null}>
+          <Form onFinish={register}>
             <CustomInput type="name" placeholder="Nume" />
             <CustomInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Parola" />
@@ -28,7 +57,8 @@ export const Register = () => {
             <Typography.Text>
               Deja sunteti inregistrat ? <Link to={Paths.login}>Intrati</Link>
             </Typography.Text>
-          </Space>
+            <ErrorMessage message={error} />
+          </Space>          
         </Card>
       </Row>
     </Layout>
